694debbec965923c51791f142f2c8553e39b9fff
Neil Finlay <finbsp@gmail.com>
2020-12-03 22:25:38 +0000

    Added 3 hidden buttons. reset BL to 3 levels

--- keyboards/xd87/keymaps/finlaybob/keymap.c
+++ keyboards/xd87/keymaps/finlaybob/keymap.c
@@ -15,9 +15,10 @@
  */
 #include QMK_KEYBOARD_H
 
-enum custom_keycodes 
+enum custom_keycodes
 {
     GIT_STA = SAFE_RANGE,
+    GIT_COD,
 };
 
 enum Layer
@@ -30,32 +31,32 @@ enum Layer
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [BASE_LAYER] = LAYOUT_tkl_iso(
-          KC_ESC,             KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,    KC_F10,     KC_F11,   KC_F12,       KC_PSCR,  KC_SLCK,  KC_PAUS,
+          KC_ESC,   KC_RGUI,    KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,    KC_F10,     KC_F11,   KC_F12,       KC_PSCR,  KC_SLCK,  KC_PAUS,
 
-          KC_GRV,   KC_1,     KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,     KC_0,       KC_MINS,  KC_EQL,   KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP, 
-          KC_TAB,   KC_Q,     KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,     KC_P,       KC_LBRC,  KC_RBRC,              KC_DEL,   KC_END,   KC_PGDN, 
-          KC_CAPS,  KC_A,     KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,     KC_SCLN,    KC_QUOT,  KC_NUHS,  KC_ENT, 
-          KC_LSFT,  KC_NUBS,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,     KC_SLSH,  KC_RSFT,                        KC_UP, 
+          KC_GRV,   KC_1,     KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,     KC_0,       KC_MINS,  KC_EQL,   KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,
+          KC_TAB,   KC_Q,     KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,     KC_P,       KC_LBRC,  KC_RBRC,              KC_DEL,   KC_END,   KC_PGDN,
+          KC_CAPS,  KC_A,     KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,     KC_SCLN,    KC_QUOT,  KC_NUHS,  KC_ENT,
+          KC_LSFT,  KC_NUBS,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,     KC_SLSH,  KC_RSFT,              KC_RGUI,   KC_UP,   KC_RGUI,
           KC_LCTL,  KC_LGUI,  KC_LALT,                        KC_SPC,                           KC_RALT,    KC_RGUI,  MO(1),    KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT
           ),
 
         [FUNC_COLOUR] = LAYOUT_tkl_iso(
-          _______, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24,                                KC_SLEP, _______,   RESET,
+          _______,KC_RGUI,KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24,                                KC_SLEP, _______,   RESET,
 
-          BL_TOGG,        BL_STEP, BL_INC, BL_DEC, BL_BRTG, _______, _______, _______, _______, _______, _______, _______, _______, VLK_TOG ,      _______, _______, _______, 
-          OSL(GIT_LAYER), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,              _______, _______, _______, 
-          _______,        RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, RGB_M_P, RGB_M_R, RGB_M_SN, RGB_M_X, _______, _______, _______, KC_MPLY, 
-          _______,        RGB_TOG, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, RGB_M_B, RGB_M_SW, RGB_M_K, RGB_M_G, _______, _______,                     KC_VOLU,
+          BL_TOGG,        BL_DEC, BL_INC, BL_BRTG, _______, _______, _______, _______, _______, _______, _______, _______, _______, VLK_TOG ,      _______, _______, _______,
+          OSL(GIT_LAYER), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,              _______, _______, _______,
+          _______,        RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, RGB_M_P, RGB_M_R, RGB_M_SN, RGB_M_X, _______, _______, _______, KC_MPLY,
+          _______,        RGB_TOG, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, RGB_M_B, RGB_M_SW, RGB_M_K, RGB_M_G, _______, _______,             KC_RGUI,KC_VOLU,KC_RGUI,
           _______,        _______, _______,           A(KC_F14),                _______, _______, _______, _______,                                  KC_MPRV, KC_VOLD, KC_MNXT
           ),
-          
-        [GIT_LAYER] = LAYOUT_tkl_iso(
-          _______, GIT_STA, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,              _______, _______, _______,
 
-          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, 
-          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,              _______, _______, _______, 
-          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         
-          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                       _______,
+        [GIT_LAYER] = LAYOUT_tkl_iso(
+          _______, _______, GIT_STA, GIT_COD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,              _______, _______, _______,
+
+          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______,
+          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,              _______, _______, _______,
+          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,              _______, _______, _______,
           _______, _______, _______,          _______,                   _______, _______, _______, _______,                                _______, _______, _______
           ),
 
@@ -65,11 +66,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case GIT_STA:
-      if (record->event.pressed) {
-        SEND_STRING("git status" SS_TAP(X_ENTER));
+      if (record->event.pressed)
+      {
+        SEND_STRING("git status --short" SS_TAP(X_ENTER));
       }
       break;
-        
+
+    case GIT_COD:
+      if (record->event.pressed)
+      {
+        SEND_STRING("git checkout dev" SS_TAP(X_TAB) SS_TAP(X_ENTER));
+      }
+      break;
+
   }
 
   return true;
