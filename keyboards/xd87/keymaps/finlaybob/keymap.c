213a18d316fa2065efa51ce121a67bcef21295bb
Neil Finlay <finbsp@gmail.com>
2020-12-04 17:23:09 +0000

    Swap FN and menu keys. Added actions on extra keys. Now just need to put switches in 😂

--- keyboards/xd87/keymaps/finlaybob/keymap.c
+++ keyboards/xd87/keymaps/finlaybob/keymap.c
@@ -17,7 +17,9 @@
 
 enum custom_keycodes
 {
-    GIT_STA = SAFE_RANGE,
+    ACTION_OPEN_WT = SAFE_RANGE,
+    ACTION_EXIT_TERM,
+    GIT_STA,
     GIT_COD,
 };
 
@@ -36,8 +38,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_GRV,   KC_1,     KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,     KC_0,       KC_MINS,  KC_EQL,   KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,
           KC_TAB,   KC_Q,     KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,     KC_P,       KC_LBRC,  KC_RBRC,              KC_DEL,   KC_END,   KC_PGDN,
           KC_CAPS,  KC_A,     KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,     KC_SCLN,    KC_QUOT,  KC_NUHS,  KC_ENT,
-          KC_LSFT,  KC_NUBS,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,     KC_SLSH,  KC_RSFT,              KC_RGUI,   KC_UP,   KC_RGUI,
-          KC_LCTL,  KC_LGUI,  KC_LALT,                        KC_SPC,                           KC_RALT,    KC_RGUI,  MO(1),    KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT
+          KC_LSFT,  KC_NUBS,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,     KC_SLSH,  KC_RSFT,              ACTION_EXIT_TERM,   KC_UP,   ACTION_OPEN_WT,
+          KC_LCTL,  KC_LGUI,  KC_LALT,                        KC_SPC,                           KC_RALT,    MO(1),  KC_APP,    KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT
           ),
 
         [FUNC_COLOUR] = LAYOUT_tkl_iso(
@@ -65,6 +67,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    case ACTION_OPEN_WT:
+      if (record->event.pressed)
+      {
+        SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_R) SS_UP(X_LGUI) SS_DELAY(50) "wt" SS_TAP(X_ENTER));
+      }
+      break;
+    case ACTION_EXIT_TERM:
+      if (record->event.pressed)
+      {
+        SEND_STRING("exit" SS_TAP(X_ENTER));
+      }
+      break;
+
     case GIT_STA:
       if (record->event.pressed)
       {
