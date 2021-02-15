ae9a58e8a5f5f278f05dfe19ab76cd408d7bc555
Neil Finlay <finbsp@gmail.com>
2021-02-15 15:21:28 +0000

    Config changes

--- keyboards/sofle/keymaps/finlaybob/keymap.c
+++ keyboards/sofle/keymaps/finlaybob/keymap.c
@@ -238,78 +238,78 @@ static void render_wpm_graph(void) {
 	wpm_graph_timer = timer_read();
 	return;
     }
-    if(timer_elapsed(wpm_graph_timer) > 500) {
-	wpm_graph_timer = timer_read();
+    if(timer_elapsed(wpm_graph_timer) > 100) {
+        wpm_graph_timer = timer_read();
 
-	if(OLED_DISPLAY_HEIGHT == 64)
-		bar_height = get_current_wpm() / 2;
-	if(OLED_DISPLAY_HEIGHT == 32)
-		bar_height = get_current_wpm() / 4;
-	if(bar_height > OLED_DISPLAY_HEIGHT)
-		bar_height = OLED_DISPLAY_HEIGHT;
+        if(OLED_DISPLAY_HEIGHT == 64)
+            bar_height = get_current_wpm() / 2;
+        if(OLED_DISPLAY_HEIGHT == 32)
+            bar_height = get_current_wpm() / 4;
+        if(bar_height > OLED_DISPLAY_HEIGHT)
+            bar_height = OLED_DISPLAY_HEIGHT;
 
-	if(bar_height == 0) {
-	    // keep track of how many zero bars we have drawn.  If
-	    // there is a whole screen worth, turn the display off and
-	    // wait until there is something to do
-	    if (zero_bar_count > OLED_DISPLAY_WIDTH) {
-		oled_off();
-		return;
-	    }
-	    zero_bar_count++;
-	} else
-	    zero_bar_count=0;
+        if(bar_height == 0) {
+            // keep track of how many zero bars we have drawn.  If
+            // there is a whole screen worth, turn the display off and
+            // wait until there is something to do
+            if (zero_bar_count > OLED_DISPLAY_WIDTH) {
+            oled_off();
+            return;
+            }
+            zero_bar_count++;
+        } else
+            zero_bar_count=0;
 
-	oled_pan(false);
-	bar_count++;
-	for (uint8_t i = (OLED_DISPLAY_HEIGHT / 8); i > 0; i--) {
-	    if (bar_height > 7) {
-		if (i % 2 == 1 && bar_count % 3 == 0)
-		    bar_segment = 254;
-		else
-		    bar_segment = 255;
-		bar_height -= 8;
-	    } else {
-		switch (bar_height) {
-		    case 0:
-			bar_segment = 0;
-			break;
+        oled_pan(false);
+        bar_count++;
+        for (uint8_t i = (OLED_DISPLAY_HEIGHT / 8); i > 0; i--) {
+            if (bar_height > 7) {
+            if (i % 2 == 1 && bar_count % 3 == 0)
+                bar_segment = 254;
+            else
+                bar_segment = 255;
+            bar_height -= 8;
+            } else {
+            switch (bar_height) {
+                case 0:
+                bar_segment = 0;
+                break;
 
-		    case 1:
-			bar_segment = 128;
-			break;
+                case 1:
+                bar_segment = 128;
+                break;
 
-		    case 2:
-			bar_segment = 192;
-			break;
+                case 2:
+                bar_segment = 192;
+                break;
 
-		    case 3:
-			bar_segment = 224;
-			break;
+                case 3:
+                bar_segment = 224;
+                break;
 
-		    case 4:
-			bar_segment = 240;
-			break;
+                case 4:
+                bar_segment = 240;
+                break;
 
-		    case 5:
-			bar_segment = 248;
-			break;
+                case 5:
+                bar_segment = 248;
+                break;
 
-		    case 6:
-			bar_segment = 252;
-			break;
+                case 6:
+                bar_segment = 252;
+                break;
 
-		    case 7:
-			bar_segment = 254;
-			break;
-		}
-		bar_height = 0;
+                case 7:
+                bar_segment = 254;
+                break;
+            }
+            bar_height = 0;
 
-		if (i % 2 == 1 && bar_count % 3 == 0)
-		    bar_segment++;
-	    }
-	    oled_write_raw_byte(bar_segment, (i-1) * OLED_DISPLAY_WIDTH);
-	}
+            if (i % 2 == 1 && bar_count % 3 == 0)
+                bar_segment++;
+            }
+            oled_write_raw_byte(bar_segment, (i-1) * OLED_DISPLAY_WIDTH);
+        }
     }
 }
 
@@ -671,11 +671,8 @@ void enc_update_default(uint8_t index, bool cw){
             tap_code(KC_VOLD);
         }
     } else if (index == RIGHT) {
-        if (cw) {
-            tap_code(KC_DOWN);
-        } else {
-            tap_code(KC_UP);
-        }
+        encoder_rgb_edit_tap(cw);
+
     }
 }
 
@@ -686,19 +683,19 @@ void enc_update_raised(uint8_t index, bool cw){
         encoder_update_super_ctrltab(cw);
 
     } else if (index == RIGHT) {
-        if (cw) {
-            tap_code(KC_RIGHT);
-        } else {
-            tap_code(KC_LEFT);
 
-        }
     }
 }
 
 void enc_update_lowered(uint8_t index, bool cw){
 
     if (index == LEFT) {
-        encoder_rgb_edit_tap(cw);
+        if (cw) {
+            tap_code(KC_PGDOWN);
+        } else {
+            tap_code(KC_PGUP);
+
+        }
     } else if (index == RIGHT) {
         encoder_update_super_alttab(cw);
     }
