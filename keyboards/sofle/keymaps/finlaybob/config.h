d750d2eb7f3b32c4bd51c86b83ab068c39e74090
Neil Finlay <finbsp@gmail.com>
2021-02-16 10:11:45 +0000

    Change to primary on RHS
    Change RGB

--- keyboards/sofle/keymaps/finlaybob/config.h
+++ keyboards/sofle/keymaps/finlaybob/config.h
@@ -1,19 +1,18 @@
 #pragma once
 
-/* The way how "handedness" is decided (which half is which),
-see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
-for more options.
-*/
+#define MASTER_RIGHT
 
 #define RGB_DI_PIN D3
-#define RGBLIGHT_SPLIT
+
 #ifdef RGB_DI_PIN
+    #define RGBLIGHT_SPLIT
+
     // #define RGBLIGHT_ANIMATIONS
-    #define RGBLIGHT_EFFECT_BREATHING
+    #define RGBLIGHT_EFFECT_TWINKLE
     #define RGBLED_NUM 8
-    #define RGBLIGHT_HUE_STEP 4
-    #define RGBLIGHT_SAT_STEP 8
-    #define RGBLIGHT_VAL_STEP 8
+    #define RGBLIGHT_HUE_STEP 1
+    #define RGBLIGHT_SAT_STEP 17
+    #define RGBLIGHT_VAL_STEP 17
 
     #define RGBLIGHT_SLEEP
 #endif
@@ -21,4 +20,3 @@ for more options.
 #define RGBLIGHT_LAYERS
 #define RGBLIGHT_MAX_LAYERS 4
 #define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
-
