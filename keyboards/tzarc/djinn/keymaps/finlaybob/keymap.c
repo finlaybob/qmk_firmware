2895b2829d25ba727ba026a4768575dc2e5c0f98
Neil <finbsp@gmail.com>
2021-06-08 16:22:10 +0100

    Reset default keymap and move to custom one

--- keyboards/tzarc/djinn/keymaps/finlaybob/keymap.c
+++ keyboards/tzarc/djinn/keymaps/finlaybob/keymap.c
@@ -20,7 +20,7 @@
 #include <backlight.h>
 #include <qp.h>
 
-#define MEDIA_KEY_DELAY 2
+#define MEDIA_KEY_DELAY 10
 
 enum { _QWERTY, _LOWER, _RAISE, _ADJUST };
 #define KC_LWR MO(_LOWER)
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                          KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME,                          KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_END,                           KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-                                   KC_LGUI, KC_LWR,  KC_SPC,  KC_NO,                            KC_NO,   KC_SPC,  KC_RSE,  KC_LALT,
+                                   KC_LGUI, KC_LALT, KC_LWR,  KC_ENT,                            KC_SPC,  KC_RSE, KC_RCTL, KC_LALT,
                                                                       RGB_RMOD,         RGB_MOD,
                                                      KC_UP,                                              KC_UP,
                                             KC_LEFT, _______, KC_RIGHT,                         KC_LEFT, _______, KC_RIGHT,
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_RAISE] = LAYOUT_all(
         KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                         _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______,_______, KC_UP,    _______, _______, _______, _______,                         _______, _______, _______, _______, _______, _______, _______,
+        CK_TOGG,_______, KC_UP,    _______, _______, _______, _______,                         _______, _______, _______, _______, _______, _______, _______,
         _______,KC_LEFT, KC_DOWN, KC_RIGHT, _______, KC_UNDS, KC_NO,                           KC_NO,   KC_EQL,  _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, KC_MINS, KC_NO,                           KC_NO,   KC_PLUS, _______, _______, _______, _______, _______,
                                    _______, _______, _______, _______,                         _______, _______, _______, _______,
