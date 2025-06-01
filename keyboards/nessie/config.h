647299d9522426233dab9e8f30e6b6f70fd8cedf
Neil Finlay <finbsp@gmail.com>
2025-06-01 16:59:43 +0100

    Tidying a bit

--- keyboards/nessie/config.h
+++ keyboards/nessie/config.h
@@ -26,22 +26,14 @@
 
 #define TP_CS_PIN GP1
 
-// READY pin seems unused by the Cirque driver,
-// but when I've tried to use the trackpad
-// without it on e.g. circuitpython, it doesn't work.
-#define TP_READY_PIN GP0
-
 #define POINTING_DEVICE_DEBUG
 
-// #define DISP_CS_PIN GP5
-// #define DISP_DC_PIN GP6
-// #define DISP_RST_PIN GP7
-
-// #define DISP_TIMEOUT 30000
+#define DISP_CS_PIN GP5
+#define DISP_DC_PIN GP6
+#define DISP_RST_PIN GP7
 
 #define POINTING_DEVICE_CS_PIN TP_CS_PIN
 
-// #define CIRQUE_PINNACLE_SPI_DIVISOR 8 // Removed to use driver default (64)
 #define CIRQUE_PINNACLE_SPI_LSBFIRST false
 #define CIRQUE_PINNACLE_SPI_CS_PIN TP_CS_PIN
 #define CIRQUE_PINNACLE_DIAMETER_MM 40
--- keyboards/nessie/nessie.c
+++ keyboards/nessie/nessie.c
@@ -4,12 +4,8 @@
 #include QMK_KEYBOARD_H
 #include "custom_keys.h"
 
-void keyboard_post_init_user(void) {
-    printf("NESS_DBG: keyboard_post_init_user() CALLED\n");
-}
+void keyboard_post_init_kb(void) {
 
-void pointing_device_init_user(void) {
-    printf("NESS_DBG: pointing_device_init_user() CALLED\n");
 }
 
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
@@ -23,12 +19,11 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
 }
 
 report_mouse_t pointing_device_task_kb(report_mouse_t mouse_report) {
-    #ifdef CONSOLE_ENABLE
-    static uint32_t last_task_print_time = 0; // Keep this specific to the task
-    if (timer_elapsed32(last_task_print_time) > 2000) { // Print every 2 seconds
-        printf("Nessie pointing device task running (from nessie.c task V2).\n");
-        last_task_print_time = timer_read32();
-    }
-    #endif
+    // static uint32_t last_task_print_time = 0; // Keep this specific to the task
+    // if (timer_elapsed32(last_task_print_time) > 2000) { // Print every 2 seconds
+    //     printf("Nessie pointing device task running (from nessie.c task V2).\n");
+    //     last_task_print_time = timer_read32();
+    // }
     return mouse_report;
 }
+
