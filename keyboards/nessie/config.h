55caeeab1e5a178ece8ca1f1bd3161a7a74b4316
Neil Finlay <finbsp@gmail.com>
2025-06-04 20:03:56 +0100

    Tidying

--- keyboards/nessie/config.h
+++ keyboards/nessie/config.h
@@ -4,11 +4,19 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
 
 #ifdef BACKLIGHT_ENABLE
+#define BACKLIGHT_BREATHING
+#define BREATHING_PERIOD 10
 #   define BACKLIGHT_PWM_DRIVER PWMD4
 #   define BACKLIGHT_PWM_CHANNEL RP2040_PWM_CHANNEL_A
+#   define BACKLIGHT_PIN GP8
+// multiple pins are not supported on hardware PWM
 #endif
 
 #ifdef ENCODER_ENABLE
+#   ifdef POINTING_DEVICE_ENABLE
+#       error "Pointing device and encoder cannot be enabled at the same time."
+#   endif
+#   define ENCODER_A_PIN GP2
 #   define ENCODER_A_PIN GP0
 #   define ENCODER_B_PIN GP1
 #   define ENCODER_RESOLUTION 4
@@ -25,12 +33,25 @@
 #define SPI_MISO_PIN GP4
 
 #ifdef POINTING_DEVICE_ENABLE
+#   ifdef ENCODER_ENABLE
+#       error "Pointing device and encoder cannot be enabled at the same time."
+#   endif
+
 #   define TP_CS_PIN GP1
+#   define TP_READY_PIN GP0 // UNUSED
+
 #   define POINTING_DEVICE_CS_PIN TP_CS_PIN
-#   define CIRQUE_PINNACLE_SPI_LSBFIRST false
-#   define CIRQUE_PINNACLE_SPI_CS_PIN TP_CS_PIN
-#   define CIRQUE_PINNACLE_DIAMETER_MM 40
+
+// Ready pin is optional but not able to be used with inertial cursor
+//#   define POINTING_DEVICE_MOTION_PIN TP_READY_PIN
+
 #   define POINTING_DEVICE_DEBUG
+#   define CIRQUE_PINNACLE_DIAMETER_MM 40
+#   define CIRQUE_PINNACLE_TAP_ENABLE
+#   define POINTING_DEVICE_HIRES_SCROLL_ENABLE
+#   define POINTING_DEVICE_GESTURES_SCROLL_ENABLE
+#   define POINTING_DEVICE_HIRES_SCROLL_MULTIPLIER 20
+#   define POINTING_DEVICE_GESTURES_CURSOR_GLIDE_ENABLE
 #endif //POINTING_DEVICE_ENABLE
 
 
@@ -53,3 +74,5 @@
 #define TRI_LAYER_LOWER_LAYER _LWR
 #define TRI_LAYER_UPPER_LAYER _RSE
 #define TRI_LAYER_ADJUST_LAYER _ADJ
+
+
