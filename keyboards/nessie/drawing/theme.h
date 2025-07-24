eb7f79c3d82bab313a2535b4247ed4b07b99a3fc
NeilF <finbsp@gmail.com>
2025-07-24 15:22:01 +0100

    save changes

---
+++ keyboards/nessie/drawing/theme.h
@@ -0,0 +1,9 @@
+// Copyright © 2025 Neil Finlay <finbsp@gmail.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define ND_THEME_BG 0,0,0
+#define ND_THEME_FG 0,0,255
+#define ND_THEME_ACCENT_A 132, 92, 137
+#define ND_THEME_ACCENT_B 37, 183, 252
--- keyboards/nessie/drawing/widget.c
+++ keyboards/nessie/drawing/widget.c
@@ -6,33 +6,19 @@
 static widget_t widget[MAX_WIDGETS];
 static uint8_t  widget_count = 0;
 
-widget_handle_t thsl_create_widget(uint16_t x, uint16_t y, uint16_t width, uint16_t height,
-                                   painter_image_handle_t icon, const char *label,
-                                   bool border, painter_font_handle_t font,
-                                   uint8_t hue, uint8_t sat, uint8_t val) {
-    // static widget_t widget = {0};
-    // widget.x      = x;
-    // widget.y      = y;
-    // widget.width  = width;
-    // widget.height = height;
-    // widget.icon   = icon;
-    // widget.label  = label;
-    // widget.border = border;
-    // widget.font   = font;
-    // widget.hue    = hue;
-    // widget.sat    = sat;
-    // widget.val    = val;
-
-    // return &widget;
-
-    if (widget_count >= 8) {
+widget_t *thsl_create_widget(uint16_t x, uint16_t y, uint16_t width, uint16_t height, painter_image_handle_t icon, const char *label, bool border, painter_font_handle_t font, uint8_t hue, uint8_t sat, uint8_t val) {
+    if (widget_count >= MAX_WIDGETS) {
         return NULL; // No more widgets can be created
     }
 
     widget[widget_count].id     = widget_count; // Assign a unique ID
     widget[widget_count].x      = x;
     widget[widget_count].y      = y;
-    widget[widget_count].width  = width;
+    if (width == 0) {
+        widget[widget_count].width = qp_textwidth(font, label) + 42; // Update width based on text length
+    } else {
+        widget[widget_count].width = width;
+    }
     widget[widget_count].height = height;
     widget[widget_count].icon   = icon;
     widget[widget_count].label  = label;
@@ -41,20 +27,26 @@ widget_handle_t thsl_create_widget(uint16_t x, uint16_t y, uint16_t width, uint1
     widget[widget_count].hue    = hue;
     widget[widget_count].sat    = sat;
     widget[widget_count].val    = val;
+    widget[widget_count].dirty  = true; // Mark the widget as dirty for redrawing
 
     widget_count++;
     return &widget[widget_count - 1];
 }
 
-void thsl_widget_destroy(widget_handle_t widget){
-    // Currently, nothing to do here as we are using a static widget.
-    // If dynamic memory allocation was used, this would free the memory.
+void thsl_widget_destroy(widget_t *widget) {
+    // This needs to be implemented better with dynamic memory management
+    // For now, we just reset the widget count and mark the widget as NULL
+
     if (widget == NULL) {
         return; // Nothing to destroy
     }
+
+    widget_count--;
+    widget = NULL;
 }
 
-void thsl_widget_draw(painter_device_t device, widget_handle_t widget) {
+void thsl_widget_draw(painter_device_t device, widget_t *widget) {
+    qp_rect(device, widget->x, widget->y, widget->x + widget->width, widget->y + widget->height, HSV_WHITE, false);
     // Draw the background rectangle
     qp_rect(device, widget->x, widget->y, widget->x + widget->width, widget->y + widget->height, HSV_WHITE, false);
 
@@ -70,4 +62,22 @@ void thsl_widget_draw(painter_device_t device, widget_handle_t widget) {
         uint16_t text_y = widget->y + (widget->height / 2) - (widget->font->line_height / 2);
         qp_drawtext_recolor(device, text_x, text_y, widget->font, widget->label, widget->hue, widget->sat, widget->val, HSV_BLACK);
     }
+
+    widget->dirty = false; // Mark the widget as clean after drawing
+}
+
+void thsl_widget_set_label(widget_t *widget, const char *label) {
+    if (widget == NULL || label == NULL) {
+        return;
+    }
+
+    if (widget) {
+        widget->label = label;
+    }
+
+    // Update width based on text length
+    widget->width = qp_textwidth(widget->font, label) + 42;
+
+    // Mark the widget as dirty for redrawing
+    widget->dirty = true;
 }
