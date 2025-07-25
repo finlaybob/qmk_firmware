6300f18f272c58538c58fc99c37c690ac1ab48e1
NeilF <finbsp@gmail.com>
2025-07-25 14:49:06 +0100

    fix copyright info

--- keyboards/nessie/config.h
+++ keyboards/nessie/config.h
@@ -1,4 +1,4 @@
-// Copyright © 2025 Neil Finlay <finbsp@gmail.com>
+// Copyright © 2025 Neil Finlay / thslkeys <finbsp@gmail.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
--- keyboards/nessie/keymaps/default/keymap.c
+++ keyboards/nessie/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-// Copyright ©️ 2024 Neil Finlay
+// Copyright ©️ 2024 Neil Finlay / thslkeys
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
 
